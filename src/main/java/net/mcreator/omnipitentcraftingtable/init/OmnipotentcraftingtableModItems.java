cc8303e7f733496e6386522c57ce20959a70107f
FuBangkun <mc14705506502@163.com>
2023-01-16 19:14:21 +0800

    Updata

--- src/main/java/net/mcreator/omnipitentcraftingtable/init/OmnipotentcraftingtableModItems.java
+++ src/main/java/net/mcreator/omnipitentcraftingtable/init/OmnipotentcraftingtableModItems.java
@@ -10,7 +10,6 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.item.Item;
 
-import net.mcreator.omnipitentcraftingtable.item.WoodenArmorItem;
 import net.mcreator.omnipitentcraftingtable.item.TinyCoalItem;
 import net.mcreator.omnipitentcraftingtable.item.TinyCharcoalItem;
 import net.mcreator.omnipitentcraftingtable.OmnipotentcraftingtableMod;
@@ -19,9 +18,4 @@ public class OmnipotentcraftingtableModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, OmnipotentcraftingtableMod.MODID);
 	public static final RegistryObject<Item> TINY_COAL = REGISTRY.register("tiny_coal", () -> new TinyCoalItem());
 	public static final RegistryObject<Item> TINY_CHARCOAL = REGISTRY.register("tiny_charcoal", () -> new TinyCharcoalItem());
-	public static final RegistryObject<Item> WOODEN_ARMOR_HELMET = REGISTRY.register("wooden_armor_helmet", () -> new WoodenArmorItem.Helmet());
-	public static final RegistryObject<Item> WOODEN_ARMOR_CHESTPLATE = REGISTRY.register("wooden_armor_chestplate",
-			() -> new WoodenArmorItem.Chestplate());
-	public static final RegistryObject<Item> WOODEN_ARMOR_LEGGINGS = REGISTRY.register("wooden_armor_leggings", () -> new WoodenArmorItem.Leggings());
-	public static final RegistryObject<Item> WOODEN_ARMOR_BOOTS = REGISTRY.register("wooden_armor_boots", () -> new WoodenArmorItem.Boots());
 }
